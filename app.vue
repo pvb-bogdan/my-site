d60e9ddecc3f1489f2ac84938de5affd6457e1b8
Bogdan Petre <35566477+pvb-bogdan@users.noreply.github.com>
2025-06-03 14:34:24 +0300

    just tests

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-   <h1 class="text-3xl font-bold underline text-red-500 hover:text-blue-500 duration-300 ease-in-out hover:cursor-pointer">
-    Hello world!
-  </h1>
+<div class="container mx-auto">
+  <div>test</div>
+  <div>test</div>
+  <div>test</div>
+  <div>test</div>
+  <div>test</div>
+  <div>test</div>
+</div>
 </template>
+<style lang="css">
+.container div {
+  width: 100px;
+  height: 100px;
+  background-color: #4a90e2;
+  margin: 10px;
+  display: inline-block;
+  border-radius: 8px;
+  display: grid;
+  place-items: center;
+  color: white;
+  font-size: 20px;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
